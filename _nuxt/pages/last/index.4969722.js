b8d9f05c8df89fa2f696e4259be0520d5b747e86
zx <593512503@qq.com>
2020-07-05 00:15:43 +0800

    secrace static

---
+++ _nuxt/pages/last/index.4969722.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{299:function(t,e,n){"use strict";n.r(e);var c={fetch:function(t){t.redirect("/last/1")}},l=n(43),component=Object(l.a)(c,(function(){var t=this.$createElement;return(this._self._c||t)("div")}),[],!1,null,null,null);e.default=component.exports}}]);
